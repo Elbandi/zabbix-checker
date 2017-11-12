f87869ce3538ad2693b6aba562eddc4f20492cc9
Andras Elso <elso.andras@gmail.com>
2017-11-12 02:32:02 +0100

    [bitcoin-mining-checker]: Utolso block cimzett lekerdezese

--- bitcoin-mining-checker/main.go
+++ bitcoin-mining-checker/main.go
@@ -113,6 +113,58 @@ func GetNetworkHashPS(request []string) (uint64, error) {
 	return 0, ErrAlgoNotFound
 }
 
+func GetLastRecipient(request []string) (string, error) {
+	connCfg := &rpcclient.ConnConfig{
+		Host:         Hostname + ":" + strconv.Itoa(Port),
+		User:         Username,
+		Pass:         Password,
+		HTTPPostMode: true, // Bitcoin core only supports HTTP POST mode
+		DisableTLS:   true, // Bitcoin core does not provide TLS by default
+	}
+	// Notice the notification parameter is nil since notifications are
+	// not supported in HTTP POST mode.
+	client, err := rpcclient.New(connCfg, nil)
+	if err != nil {
+		return "", err
+	}
+	defer client.Shutdown()
+
+	var blockCount int64
+	if len(request) > 0 {
+		blockCount, err = strconv.ParseInt(request[0], 10, 0)
+		if err != nil {
+			return "", err
+		}
+	} else {
+		blockCount, err = client.GetBlockCount()
+		if err != nil {
+			return "", err
+		}
+	}
+	for ; blockCount > 0; blockCount-- {
+		blockHash, err := client.GetBlockHash(blockCount)
+		if err != nil {
+			return "", err
+		}
+		block, err := client.GetBlockVerbose(blockHash)
+		if err != nil {
+			return "", err
+		}
+		if block.Nonce == 0 {
+			// pos block has 0 nonce value, skip
+			continue
+		}
+		for _, tx := range block.RawTx {
+			if tx.Vin[0].Coinbase == "" {
+				continue
+			}
+			return tx.Vout[0].ScriptPubKey.Addresses[0], nil
+		}
+	}
+
+	return "", errors.New("Coinbase not found")
+}
+
 func main() {
 	flag.StringVar(&Hostname, "hostname", "localhost", "Send commands to node running on host")
 	flag.IntVar(&Port, "port", 1234, "Connect to JSON-RPC port")
@@ -130,6 +182,12 @@ func main() {
 		}
 	case "networkhashps":
 		if v, err := GetNetworkHashPS(flag.Args()[1:]); err != nil {
+			log.Fatalf("Error: %s", err.Error())
+		} else {
+			fmt.Print(v)
+		}
+	case "lastrecipient":
+		if v, err := GetLastRecipient(flag.Args()[1:]); err != nil {
 			log.Fatalf("Error: %s", err.Error())
 		} else {
 			fmt.Print(v)
